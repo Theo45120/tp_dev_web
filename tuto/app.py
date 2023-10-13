d4162e3da41b69d2e71e6dc5675475bd9d1d3edf
CharlesMassuard <charles.massuard@gmail.com>
2023-10-13 12:14:01 +0200

    Ajout d'une clé secrete aléatoire

--- tuto/app.py
+++ tuto/app.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 bootstrap = Bootstrap5(app)
+app.config["SECRET_KEY"] = "3b117cd7-bb11-4ff7-88b7-37e234f4d831"
 
 import os.path
 def mkpath (p):
