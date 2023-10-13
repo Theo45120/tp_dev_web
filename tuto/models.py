1ba75f2a0d1d93e2c4a9dabeb151db2fb2075900
CharlesMassuard <charles.massuard@gmail.com>
2023-10-13 11:33:21 +0200

    Ajout repr pour les classes Author & Bool

--- tuto/models.py
+++ tuto/models.py
@@ -4,6 +4,9 @@ class Author (db.Model ):
     id = db.Column(db.Integer, primary_key =True)
     name = db.Column(db.String(100))
 
+    def __repr__ (self ):
+        return "<Author (%d) %s>" % (self.id , self.name)
+
     
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key =True)
@@ -13,6 +16,9 @@ class Book(db.Model):
     img = db.Column(db.String(100))
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship ("Author", backref=db.backref ("books", lazy="dynamic"))
+
+    def __repr__ (self ):
+        return "<Book (%d) %s>" % (self.id , self.title)
 
 def get_sample():
     return Book.query.limit(10).all()
