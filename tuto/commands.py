dca21ef0277b8fb8c9cbd8c03cbb632af718b56f
CharlesMassuard <charles.massuard@gmail.com>
2023-10-20 10:01:01 +0200

    Ajout utilisateur

--- tuto/commands.py
+++ tuto/commands.py
@@ -40,4 +40,17 @@ def loaddb(filename):
 @app.cli. command ()
 def syncdb ():
     """Creates all missing tables."""
-    db.create_all ()
+    db.create_all()
+
+@app.cli.command()
+@click.argument("username")
+@click.argument("password")
+def newuser(username, password):
+    """Adds a new user."""
+    from .models import User
+    from hashlib import sha256
+    m = sha256()
+    m.update(password.encode())
+    u = User(username=username, password=m.hexdigest())
+    db.session.add(u)
+    db.session.commit()
